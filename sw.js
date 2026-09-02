27c364159231893a5aa170d4f9d7c20a91618197
brispmhub <pmail88@gmail.com>
2026-09-02 19:41:41 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,43 @@
+const CACHE_NAME = 'toddler-mail-v12';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
 // --- Temporary Debug Logic ---
     var debugBtn = document.getElementById('debugBtn');
     var debugText = document.getElementById('debugText');
@@ -24,3 +64,4 @@
         window.speechSynthesis.speak(testUtterance);
       }
     });
+});
